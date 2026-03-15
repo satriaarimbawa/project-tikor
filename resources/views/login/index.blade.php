f25233ed9d487ad693b89896c72ec0fcdcb51236
satria arimbawa <satriaarimbawa22@gmail.com>
2026-03-15 12:35:05 +0800

    second commit setting routes fisrt

---
+++ resources/views/login/index.blade.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>DASHBOARD||LOGIN</title>
+</head>
+<body>
+    <a href="/dashboard-admin">mau ke admin ?</a>
+    <hr>
+    <a href="/dashboard-operator">mau ke operator ?</a>
+</body>
+</html>
--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,9 @@
 use App\Http\Controllers\AdminController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\UserController;
+use Illuminate\Auth\Events\Login;
+use App\Http\Controllers\LoginController;
+use App\Http\Controllers\OperatorController;
 
 Route::get('/', function () {
     return view('welcome');
@@ -10,3 +13,9 @@ Route::get('/', function () {
 
 Route::get('/test-store', [UserController::class, 'store']);
 Route::get('/dashboard-admin', [AdminController::class, 'index']);
+Route::get('/dashboard-login', [LoginController::class, 'index']);
+Route::get('/dashboard-operator', [OperatorController::class, 'index']);
+
+Route::get('/', function () {
+    return view('landing');
+});
