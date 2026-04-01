0dc9e794b5197a0a2e082d535fe977edce5902aa
satria arimbawa <satriaarimbawa22@gmail.com>
2026-04-01 18:05:35 +0800

    perubahan kode

--- resources/views/landing.blade.php
+++ resources/views/landing.blade.php
@@ -5,6 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Uji Petik - Dishub Klungkung</title>
 
+    <link rel="icon" type="image/png" href="{{ asset('assets/logo_dishub.png') }}">
     <link rel="stylesheet" href="{{ asset('css/landing.css') }}">
     <link
         href="https://fonts.googleapis.com/css2?family=Playfair+Display:wght@700;900&family=Inter:wght@400;600&display=swap"
