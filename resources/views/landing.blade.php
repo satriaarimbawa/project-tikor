b01589e98ce3266ae92603334434e5fa97363d1c
hariniayu <hariningsihayu@gmail.com>
2026-05-03 19:47:04 +0800

    Update Terbaru Landing Page

--- resources/views/landing.blade.php
+++ resources/views/landing.blade.php
@@ -1,5 +1,5 @@
 <!DOCTYPE html>
-
+<html lang="id">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -7,15 +7,13 @@
 
     <link rel="icon" type="image/png" href="{{ asset('assets/logo_dishub.png') }}">
     <link rel="stylesheet" href="{{ asset('css/landing.css') }}">
-    <link
-        href="https://fonts.googleapis.com/css2?family=Playfair+Display:wght@700;900&family=Inter:wght@400;600&display=swap"
-        rel="stylesheet">
-        @vite(['resources/css/app.css', 'resources/js/app.js'])
+    <link href="https://fonts.googleapis.com/css2?family=Playfair+Display:wght@700;900&family=Inter:wght@400;600&display=swap" rel="stylesheet">
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
 </head>
 
 <body>
     <div class="hero-section">
-        <header>
+       <header>
             <div class="logo-group">
                 <img src="{{ asset('assets/logo_klungkung.png') }}" alt="Logo Klungkung">
                 <img src="{{ asset('assets/logo_dishub.png') }}" alt="Logo Dishub">
@@ -30,15 +28,19 @@
 
         <main class="main-content">
             <div class="hero-text">
-                <h1>DIGITALISASI UJI<br>PETIK DISHUB</h1>
-                <a href="/login-admin" class="btn-dashboard">Masuk Ke Dashboard</a>
+            <h1>DIGITALISASI UJI<br>PETIK DISHUB</h1>
+                <div class="button-group">
+                    <a href="/login-admin" class="btn-dashboard">Login Admin</a>
+                    <a href="/login-operator" class="btn-dashboard">Login Operator</a>
+                </div>
             </div>
         </main>
 
         <footer>
-            <p>Dinas Perhubungan Kab. Klungkung 2026</p>
+            <div class="footer-container">
+                <p>Dinas Perhubungan Kab. Klungkung 2026</p>
+            </div>
         </footer>
     </div>
 </body>
-
 </html>
